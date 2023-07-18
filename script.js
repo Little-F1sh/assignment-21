87d2bcb63b8e0ce6f53a34d58756f8630ec4bc20
Little-F1sh <itsnihon1@gmail.com>
2023-07-18 11:31:11 +0600

    not done

--- script.js
+++ script.js
@@ -6,13 +6,13 @@ for(const h3 of title) {
     console.log(h3.innerText);
 };
 
-const paragraphs = document.getElementsByTagName('p');
+// const paragraphs = document.getElementsByTagName('p');
 
-console.log(paragraphs);
+// console.log(paragraphs);
 
-for(const p of paragraphs) {
-    console.log(p.innerText);
-};
+// for(const p of paragraphs) {
+//     console.log(p.innerText);
+// };
 
 const secondTitle = document.getElementById('second-Title');
 
@@ -25,3 +25,37 @@ const lastTitle = document.getElementById('last-Title');
 console.log(lastTitle);
 
 console.log(lastTitle.innerText);
+
+const blogs = document.getElementsByClassName('blog');
+
+console.log(blogs);
+
+for(const blog of blogs) {
+    console.log(blog.innerText);
+};
+
+const blogs1 = document.getElementsByClassName('blog');
+
+
+for(const blog of blogs1) {
+    blog.style.border =  '1px solid green';
+    blog.style.margin = '50px';
+    blog.style.padding = '50px';
+    blog.style.textAlign = 'center';
+};
+
+console.log(document.querySelector('.blog'));
+
+// console.log(document.querySelectorAll('.blog'));
+
+const firstTitle = document.querySelector('h3');
+
+// console.log(firstTitle);
+
+firstTitle.style.backgroundColor = 'blue';
+
+const allP = document.querySelectorAll('p');
+
+// console.log(description);
+
+allP.style.backgroundColor = 'green';
