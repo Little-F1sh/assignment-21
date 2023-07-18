8c4d2b12c05eec16639ad76cf41e59561c3c332d
Little-F1sh <itsnihon1@gmail.com>
2023-07-18 11:05:06 +0600

    getById

--- script.js
+++ script.js
@@ -1,5 +1,27 @@
 const title = document.getElementsByTagName('h3');
 
+console.log(title);
+
 for(const h3 of title) {
     console.log(h3.innerText);
 };
+
+const paragraphs = document.getElementsByTagName('p');
+
+console.log(paragraphs);
+
+for(const p of paragraphs) {
+    console.log(p.innerText);
+};
+
+const secondTitle = document.getElementById('second-Title');
+
+console.log(secondTitle);
+
+console.log(secondTitle.innerText);
+
+const lastTitle = document.getElementById('last-Title');
+
+console.log(lastTitle);
+
+console.log(lastTitle.innerText);
